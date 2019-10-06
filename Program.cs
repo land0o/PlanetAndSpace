640c5fa21c9198ae87db394d532b6fe94b3a1d30
curtis <crutchc@yahoo.com>
2019-10-05 20:23:57 -0500

    planets list practice

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PlanetAndSpaceships
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> planetList = new List<string>() { "Mercury", "Mars" };
+
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+            planetList.Insert(1, "Venus");
+            planetList.Insert(2, "Earth");
+            planetList.Add("Pluto");
+            planetList.Remove("Pluto");
+
+            List<string> lastTwoPlanets = new List<string>() { "Uranus", "Neptune" };
+
+            planetList.AddRange(lastTwoPlanets);
+
+            List<string> RockyPlanets = planetList.GetRange(0, 4);
+
+            planetList.ForEach(planets => Console.WriteLine(planets));
+            Console.WriteLine("-----------");
+            Console.WriteLine("Rocky Planets");
+            RockyPlanets.ForEach(planets => Console.WriteLine(planets));
+
+        }
+    }
+}
